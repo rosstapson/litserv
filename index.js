39dc544cadb474dcf9be3a62515cc4d82afacc8f
rosstapson <ross.tapson@gmail.com>
2018-08-16 17:32:02 +0200

    added skeleton apollo/graphql, stubbing with json-server

--- index.js
+++ index.js
@@ -1,10 +1,23 @@
 import express from 'express';
-const app = express();
+import { ApolloServer } from 'apollo-server-express'
+import { resolvers, typeDefs } from './schema'
+
+const PORT = process.env.PORT || 3500
+console.log('API Port: ' + PORT)
+const app = express()
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  playground: true
+})
+
+server.applyMiddleware({ app })
 
 app.get('/', (req, res) => {
-    res.send("zomg")
+  res.send({ hello: 'there!' })
 })
 
-app.listen(3001, () => {
-    console.log('zomg. listserv is listening intently on 3001')
-})
+app.listen(PORT, () =>
+  console.log(`Listening at http://localhost:${PORT}/graphql`)
+)
